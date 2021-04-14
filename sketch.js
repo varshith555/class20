241ce25c261b552bde8e9178aa5b7a1b8f358424
varshith555 <82503871+varshith555@users.noreply.github.com>
2021-04-14 14:33:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+ fixedRect = createSprite(200, 200, 50, 80);
+ fixedRect.shapeColor='green'
+ movingRect= createSprite(400,200,80,30);
+ movingRect.shapeColor='green'
+}
+
+function draw() {
+   background(255,255,255);  
+   movingRect.x=World.mouseX
+   movingRect.y=World.mouseY
+
+   if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 
+    && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 
+    && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2 
+    && movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 ){
+     movingRect.shapeColor='red'
+     fixedRect.shapeColor='red'
+   }
+   else{
+    movingRect.shapeColor='green'
+    fixedRect.shapeColor='green'
+   }
+   fixedRect.debug=true
+   movingRect.debug=true
+  drawSprites();
+}
